dca174762e013283dd9a88e9b9d2ce9827019115
KarBoraN <ysfduzgun92@gmail.com>
2015-07-27 16:38:31 +0300

    first upload

---
+++ selected.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# coding: utf-8
+# Yusuf Duzgun ysfduzgun92@gmail.com
+# Licanse : GPL3
+
+
+
+
+
+import os
+from gi.repository import Notify
+from BeautifulSoup import BeautifulSoup
+from urllib import urlopen
+import unidecode
+
+def searchTureng(word):	
+	url="http://www.tureng.com/search/"+word	
+	try:
+		answer = urlopen(url)
+	except:
+		return "No connection"
+	html = answer.read()
+	parsedHtml = BeautifulSoup(html)
+	trlated = parsedHtml.body.find('td', attrs={'class':'tr ts'}).text
+	trlated.encode("ascii", "ignore")
+	trlated = unidecode.unidecode(trlated)
+
+
+	return trlated
+
+
+
+selectedText = os.popen('xsel').read()
+print (selectedText)
+
+result = searchTureng(selectedText)
+print (result)
+
+Notify.init ("SelecTra")
+trlated=Notify.Notification.new ("SelecTra", result)
+trlated.show () 
+
+
+
