c6a812892bfaf242a028e7fa4d6d94b6b690b161
KarBoraN <ysfduzgun92@gmail.com>
2015-07-29 19:25:04 +0300

    update selected.py

--- selected.py
+++ selected.py
@@ -4,37 +4,32 @@
 # Yusuf Duzgun ysfduzgun92@gmail.com
 # Licanse : GPL3
 
-
-
-
-
 import os
 from gi.repository import Notify
 from BeautifulSoup import BeautifulSoup
 from urllib import urlopen
-#import unidecode
 
 def searchTureng(word):	
-	url="http://www.tureng.com/search/"+word	
-	try:
-		answer = urlopen(url)
-	except:
-		return "No connection"
-	html = answer.read()
-	soup = BeautifulSoup(html)
-
-	trlated=''
-	try:
-		table = soup.find('table')
-		td = table.findAll('td', attrs={'class':'tr ts'})
-		for val in td:
-			trlated = trlated + val.text + ','
-		
-		return trlated
-	except:
-		return "Not Found !"
+    url="http://www.tureng.com/search/"+word	
+    try:
+    	answer = urlopen(url)
+    except:
+    	return "No connection"
+    html = answer.read()
+    soup = BeautifulSoup(html)
+    
+    trlated=''
+    try:
+    	table = soup.find('table')
+    	td = table.findAll('td', attrs={'lang':'tr'})
+        for val in td[0:5]:
+    		trlated = trlated + val.text + '\n'
+                   		
+    	return trlated
+    except:
+    	return "Not Found !"
 	
-	return trlated
+    return trlated
 
 
 selectedText = os.popen('xsel').read()
