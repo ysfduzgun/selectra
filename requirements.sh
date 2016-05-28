42a08ba3a86fb415de19e3c88f201a8ddfb9d247
yusuf <ysfduzgun92@gmail.com>
2016-05-28 08:44:14 +0300

    python-gobject req added

--- requirements.sh
+++ requirements.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+   echo "Bu betigin calismasi icin root yetkisi gereklidir." 
    exit 1
 fi
 
 apt-get install xsel
 apt-get install python-bs4
+apt-get install python-gobject
